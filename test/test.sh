221933779a0049fdc0774aa86d261b20169b0f96
seahurt <haozi.vv@gmail.com>
2019-03-13 16:41:41 +0800

    feat: update test.sh

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BASE=$(dirname $DIR)
-python $BASE/bcl2fq/bcl2fq.py -i $DIR -o $DIR --cmd-only
+python $BASE/bcl2fq/bcl2fq.py -i $DIR -o $DIR --cmd-only --binpath $BASE/bin/bcl2fastq
